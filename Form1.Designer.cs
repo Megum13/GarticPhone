193e2778b5cbb8ab9e649f586ba4a280baf117c6
Megum13 <88092386+Megum13@users.noreply.github.com>
2021-07-30 15:34:20 +0300

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WevSocketScetchful
+﻿namespace GarticPicture
 {
     partial class Form1
     {
@@ -40,12 +40,15 @@
             this.label4 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -53,18 +56,18 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.LightGreen;
-            this.button1.Location = new System.Drawing.Point(4, 52);
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(19, 246);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Start";
+            this.button1.Text = "Draw";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(259, 52);
+            this.button2.Location = new System.Drawing.Point(243, 246);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -74,9 +77,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(3, 98);
+            this.button3.Location = new System.Drawing.Point(3, 91);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(215, 23);
+            this.button3.Size = new System.Drawing.Size(100, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "Connect";
             this.button3.UseVisualStyleBackColor = true;
@@ -84,7 +87,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(3, 27);
+            this.textBox1.Location = new System.Drawing.Point(3, 20);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 3;
@@ -92,14 +95,14 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(3, 72);
+            this.textBox2.Location = new System.Drawing.Point(3, 65);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(215, 20);
             this.textBox2.TabIndex = 4;
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(3, 11);
+            this.label1.Location = new System.Drawing.Point(3, 4);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 13);
             this.label1.TabIndex = 7;
@@ -108,7 +111,7 @@
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(6, 56);
+            this.label2.Location = new System.Drawing.Point(6, 49);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(212, 13);
             this.label2.TabIndex = 8;
@@ -117,7 +120,7 @@
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(177, 11);
+            this.label3.Location = new System.Drawing.Point(177, 4);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 13);
             this.label3.TabIndex = 9;
@@ -126,7 +129,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(183, 27);
+            this.textBox3.Location = new System.Drawing.Point(183, 20);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(35, 20);
             this.textBox3.TabIndex = 11;
@@ -135,44 +138,36 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(118, 7);
+            this.label4.Location = new System.Drawing.Point(152, 7);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(108, 13);
+            this.label4.Size = new System.Drawing.Size(28, 13);
             this.label4.TabIndex = 15;
-            this.label4.Text = "Текст для отправки";
+            this.label4.Text = "Text";
             // 
             // textBox5
             // 
-            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(236)))), ((int)(((byte)(255)))));
-            this.textBox5.Location = new System.Drawing.Point(4, 26);
+            this.textBox5.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox5.Location = new System.Drawing.Point(3, 23);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(330, 20);
             this.textBox5.TabIndex = 16;
             // 
             // button4
             // 
-            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(236)))), ((int)(((byte)(255)))));
-            this.button4.Location = new System.Drawing.Point(131, 52);
+            this.button4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button4.Location = new System.Drawing.Point(130, 49);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 17;
-            this.button4.Text = "Send text";
+            this.button4.Text = "Send";
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(259, 81);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(74, 20);
-            this.numericUpDown1.TabIndex = 20;
-            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
-            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(4, 104);
+            this.pictureBox1.Location = new System.Drawing.Point(19, 75);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(330, 191);
+            this.pictureBox1.Size = new System.Drawing.Size(299, 161);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 21;
             this.pictureBox1.TabStop = false;
@@ -189,6 +184,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button8);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.textBox1);
@@ -196,30 +192,84 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.textBox3);
-            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Location = new System.Drawing.Point(4, 29);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(222, 123);
+            this.panel1.Size = new System.Drawing.Size(222, 118);
             this.panel1.TabIndex = 24;
+            this.panel1.Visible = false;
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
             this.panel2.Controls.Add(this.label4);
             this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Controls.Add(this.textBox5);
-            this.panel2.Controls.Add(this.numericUpDown1);
             this.panel2.Controls.Add(this.button4);
-            this.panel2.Location = new System.Drawing.Point(0, 153);
+            this.panel2.Location = new System.Drawing.Point(0, 176);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(336, 298);
+            this.panel2.Size = new System.Drawing.Size(336, 275);
             this.panel2.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(131, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 23);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "0";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(100, 246);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(25, 23);
+            this.button6.TabIndex = 27;
+            this.button6.Text = " <";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(212, 246);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(25, 23);
+            this.button5.TabIndex = 26;
+            this.button5.Text = " >";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button7
+            // 
+            this.button7.ForeColor = System.Drawing.Color.Red;
+            this.button7.Location = new System.Drawing.Point(4, 3);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 21);
+            this.button7.TabIndex = 26;
+            this.button7.Text = "Не подключено";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(118, 91);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 23);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Disconnect";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1059, 452);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.richTextBox1);
@@ -227,7 +277,6 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "GarticPicture";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -251,11 +300,15 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
 
---
+++ SocketIO.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace GarticPicture
+{
+    class SocketIO
+    {
+        WebSocket ws;
+        Random ran = new Random();
+
+        bool isConnected = false;
+        Form1 form1;
+
+        int turnNum = 0;
+
+        public SocketIO(Form1 form1)
+        {
+            this.form1 = form1; // Получаем форму
+
+            Thread th = new Thread(() =>  // Проверка подключения
+            {
+                while (true)
+                {
+                    if (ws != null) isConnected = ws.IsAlive;
+                    else if (ws == null) isConnected = false;
+                }
+            });
+            th.IsBackground = true;
+            th.Start();
+
+            Thread th2 = new Thread(() => // Проверка соеденения с сокетом
+            {
+                while (true)
+                {
+                    if (isConnected)
+                    {
+                        ws.Send("3");
+                    }
+                    Thread.Sleep(25000);
+                }
+            });
+            th2.IsBackground = true;
+            th2.Start();
+        }
+
+
+        public string Connect(string socket , string nick, string roomCode)
+        {
+            if (isConnected) return Disconnect();
+
+            ws = new WebSocket($"wss://sv-{socket}.garticphone.com/socket.io/?EIO=4&transport=websocket");
+
+            ws.OnMessage -= MessageSocket;
+            ws.OnMessage += MessageSocket;
+
+            try 
+            { 
+                ws.Connect();
+            }
+            catch
+            {
+                form1.ConnectInformationButton(false);
+                return "Подключение не удалось";
+            }
+
+            ws.Send("40");
+            ws.Send($"42[1,\"{"bfcc9fd4-e2a2-44db-a6e4-0d05eed1fb0e" + ran.Next(0, 1000)}\",\"{nick}\",{ran.Next(1, 30)},\"ru\",false,\"{roomCode}\"]");
+
+            form1.ConnectInformationButton(true);
+            return "Подключено";
+        }
+
+        public string Disconnect()
+        {
+            if (!isConnected) return "Нет подключения";
+
+            ws.Close();
+            form1.ConnectInformationButton(false);
+            return "Отключено";
+        }
+
+        public string Send(string text)
+        {
+            if (!isConnected) return "Нет подключения";
+
+            ws.Send(text);
+
+            return "Отправлено";
+        }
+
+        public string DrawImage(Image img)
+        {
+            if (!isConnected) return "Нет подключения";
+
+            Thread th = new Thread(() => 
+            {
+                Bitmap btm = new Bitmap(img, 758, 424);
+
+                for (int Y = 0; Y < 424; Y += 2)
+                {
+                    for (int X = 0; X < 758; X += 2)
+                    {
+
+                        var pixel = btm.GetPixel(X, Y);
+
+                        string hex = pixel.R.ToString("X2") + pixel.G.ToString("X2") + pixel.B.ToString("X2");
+
+                        if (!(pixel.R == 255 && pixel.G == 255 && pixel.B == 255))
+                        {
+                            ws.Send($"42[2,7,{{\"t\":{turnNum},\"v\":[6,[\"#{hex}\",1,1],[{X},{Y}] , [{X + 2},{Y + 2}]]}}]");
+                        }
+
+                    }
+                }
+
+                ws.Send("42[2,15,true]");
+                form1.SendToTextBox("Изображение успешно отправлено");
+            });
+            th.IsBackground = true;
+            th.Start();
+
+            return "Отправление изображения...";
+
+        }
+
+        private void MessageSocket(object sender, MessageEventArgs e)
+        {
+
+            if (e.IsText)
+            {
+                char[] charData = e.Data.ToCharArray();
+
+                if (e.Data != null)
+                {
+                    form1.SendToTextBox(e.Data);
+                }
+
+                if (charData.Length >= 21 && charData[5] == '1' && charData[6] == '1')
+                {
+                    string[] a = e.Data.Split(',');
+                    string[] b = a[2].Split(':');
+                    turnNum = Int32.Parse(b[1].ToString());
+                }
+
+            }
+
+        }
+
+    }
+}
